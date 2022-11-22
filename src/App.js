9845bd0ebd188a5f00292a2894ffbf504675d85d
safayet <safayetdib@gmail.com>
2022-11-22 13:18:17 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useCallback, useRef, useState } from 'react';
+import useBookSearch from './useBookSearch';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [query, setQuery] = useState('');
+	const [pageNumber, setPageNumber] = useState(1);
+	const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
+	const observer = useRef();
+	const lastBookElementRef = useCallback(
+		(node) => {
+			if (loading) return;
+			if (observer.current) observer.current.disconnect();
+			observer.current = new IntersectionObserver((entries) => {
+				if (entries[0].isIntersecting && hasMore) {
+					setPageNumber((prevPageNumber) => prevPageNumber + 1);
+					console.log('hell yeah');
+				}
+			});
+			if (node) observer.current.observe(node);
+		},
+		[loading, hasMore]
+	);
+
+	const handleSearch = (e) => {
+		setQuery(e.target.value);
+		setPageNumber(1);
+	};
+
+	return (
+		<main className="container">
+			<h2>INFINITE SCROLLING WITH REACT</h2>
+			<p>
+				Powered By{' '}
+				<a
+					href="https://openlibrary.org/"
+					target="_blank"
+					rel="noopener noreferrer">
+					Open Library API
+				</a>
+			</p>
+			<input
+				className="searchBar"
+				type="text"
+				value={query}
+				onChange={handleSearch}
+				placeholder="Enter Book Name Here"
+			/>
+			{books.map((book, index) => {
+				if (books.length - 20 === index + 1) {
+					return (
+						<div className="bookList" ref={lastBookElementRef} key={book}>
+							{book}
+						</div>
+					);
+				} else {
+					return (
+						<div className="bookList" key={book}>
+							{book}
+						</div>
+					);
+				}
+			})}
+			{loading && <div className="loader">Loading...</div>}
+			{error && <div className="displayError">Error</div>}
+		</main>
+	);
 }
 
 export default App;
